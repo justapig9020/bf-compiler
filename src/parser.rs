2822cbe0e5d6e8bca96f3697921396b6e7e36ab6
justapig9020 <justapig9020@gmail.com>
2024-04-02 10:02:35 +0800

    Add id and num parsing

--- src/parser.rs
+++ src/parser.rs
@@ -10,8 +10,10 @@
 // - If: if Flag { Function } [else { Function }]!
 // - While: while Flag { Function }
 // - Assign: ID = NUMBER
-// - Flag: Bool [&& Flag]*
-// - Bool: ID != NUMBER
+// - Bool: Compare [&& Compare]*
+// - Compare: Equal | NotEqual
+// - Equal: ID == NUMBER
+// - NotEqual: ID != NUMBER
 // - Move: ID("move_right") | ID("move_left")
 
 use crate::scanner::{Token, TokenStream};
@@ -24,21 +26,47 @@ struct AST<'a> {
 
 #[derive(Debug)]
 enum Statement<'a> {
-    IF(Flag<'a>, Vec<Statement<'a>>, Option<Vec<Statement<'a>>>),
-    WHILE(Flag<'a>, Vec<Statement<'a>>),
+    IF(Bool<'a>, Vec<Statement<'a>>, Option<Vec<Statement<'a>>>),
+    WHILE(Bool<'a>, Vec<Statement<'a>>),
     Assign(Token<'a>, Token<'a>),
     Move(Direction),
 }
 
 #[derive(Debug)]
-struct Flag<'a> {
-    flags: Vec<Bool<'a>>,
+struct Bool<'a> {
+    compares: Vec<Compare<'a>>,
 }
 
 #[derive(Debug)]
-struct Bool<'a> {
-    id: Token<'a>,
-    num: Token<'a>,
+enum Compare<'a> {
+    EQ(ID<'a>, u8),
+    NE(ID<'a>, u8),
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+struct Num(u8);
+
+impl<'a> TryFrom<Token<'a>> for Num {
+    type Error = anyhow::Error;
+    fn try_from(token: Token<'a>) -> Result<Self> {
+        let Token::NUM(num) = token else {
+            return Err(anyhow!("Expected NUM, found {:?}", token));
+        };
+        Ok(Self(num.parse().unwrap()))
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+struct ID<'a>(&'a str);
+
+impl<'a> TryFrom<Token<'a>> for ID<'a> {
+    type Error = anyhow::Error;
+    fn try_from(token: Token<'a>) -> Result<Self> {
+        let Token::ID(id) = token else {
+            return Err(anyhow!("Expected ID, found {:?}", token));
+        };
+        Ok(Self(id))
+    }
 }
 
 #[derive(Debug, PartialEq, Copy, Clone)]
@@ -70,7 +98,17 @@ pub fn parse(tokens: TokenStream) -> Result<AST> {
 #[cfg(test)]
 mod parser {
     use super::*;
-
+    // Semantic:
+    // - Function: [Statement]*
+    // - Statement: If | While | Assign | Move
+    // - If: if Flag { Function } [else { Function }]!
+    // - While: while Flag { Function }
+    // - Assign: ID = NUMBER
+    // - Bool: Compare [&& Compare]*
+    // - Compare: Equal | NotEqual
+    // - Equal: ID == NUMBER
+    // - NotEqual: ID != NUMBER
+    // - Move: ID("move_right") | ID("move_left")
     #[test]
     fn test_parse_direction() {
         let testcase = [
@@ -80,6 +118,36 @@ mod parser {
         ];
         for (token, expect) in testcase.into_iter() {
             let output = Direction::try_from(token);
+            if let Ok(expect) = expect {
+                assert_eq!(output.unwrap(), expect);
+            } else {
+                assert!(output.is_err());
+            }
+        }
+    }
+    #[test]
+    fn test_parse_id() {
+        let testcase = [
+            (Token::ID("hello"), Ok(ID("hello"))),
+            (Token::NUM("123"), Err(())),
+        ];
+        for (token, expect) in testcase.into_iter() {
+            let output = ID::try_from(token);
+            if let Ok(expect) = expect {
+                assert_eq!(output.unwrap(), expect);
+            } else {
+                assert!(output.is_err());
+            }
+        }
+    }
+    #[test]
+    fn test_parse_num() {
+        let testcase = [
+            (Token::NUM("123"), Ok(Num(123))),
+            (Token::ID("hello"), Err(())),
+        ];
+        for (token, expect) in testcase.into_iter() {
+            let output = Num::try_from(token);
             if let Ok(expect) = expect {
                 assert_eq!(output.unwrap(), expect);
             } else {
